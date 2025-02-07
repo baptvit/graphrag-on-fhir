56490888849db457d1a701769cb6710d7a014ade
baptvit <jvrbaptista@live.com>
2025-02-07 20:52:20 -0300

    feat: changes overall

--- config.py
+++ config.py
@@ -48,9 +48,9 @@ safety_settings = {
 # llm = ChatVertexAI(model=LLM_MODEL, temperature=0, safety_settings=safety_settings)
 
 # Azure Configurations
-LLM_MODEL = "gpt-4o-2024-08-06"
+#LLM_MODEL = "gpt-4o-2024-08-06"
 # LLM_MODEL = "anthropic.claude-v3-opus"
-# LLM_MODEL = "gpt-4o-2024-11-20"
+LLM_MODEL = "gpt-4o-2024-11-20"
 llm = AzureChatOpenAI(
     api_key=OPENAI_API_KEY,
     azure_deployment=LLM_MODEL,
@@ -90,7 +90,7 @@ SIMILARITY_THRESHOLD = 0.84
 K = 100000  # no restriction pratical
 
 # User
-CONSUMER_ID = "Beatris270_Bogan287_5b3645de-a2d0-d016-0839-bab3757c4c58"
+CONSUMER_ID = "Allen322_Ferry570_ad134528-56a5-35fd-c37f-466ff119c625"
 
 INPUT_QUESTION = {}
 
